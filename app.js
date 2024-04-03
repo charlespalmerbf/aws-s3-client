7c47d4412a4f896c0034c1fae11489a3ce6fd7d6
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2024-04-03 18:57:01 +0100

    fix: resolving issues with upload route and cleaning up comments

--- app.js
+++ app.js
@@ -1,37 +1,51 @@
-// app.js
-
 const express = require("express");
 const dotenv = require("dotenv");
 const multer = require("multer");
 const AWS = require("aws-sdk");
-const multerS3 = require("multer-s3");
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// s3 configuration
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION,
 });
 
-const upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: process.env.AWS_BUCKET_NAME,
-        contentType: multerS3.AUTO_CONTENT_TYPE,
-        acl: "public-read",
-        key: function (req, file, cb) {
-            cb(null, Date.now().toString() + "-" + file.originalname);
-        },
-    }),
-});
+// Multer configuration
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 // File upload route
-app.post("/upload", upload.single("file"), (req, res, next) => {
-    res.status(200).send("File uploaded successfully");
+
+app.post("/upload", upload.single("file"), function (req, res) {
+    const file = req.file;
+
+    // Check if file has been correctly passed
+    if (!file) {
+        return res.status(400).send("Please select a file to upload");
+    }
+
+    // Set s3 bucket and key (filename) where the file will be stored
+    const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: Date.now() + "-" + file.originalname,
+        Body: file.buffer,
+    };
+
+    // Upload file to S3
+    s3.upload(params, function (err, data) {
+        if (err) {
+            console.error(err);
+            return res.status(500).send("Error uploading file to s3");
+        }
+
+        // If file upload to S3 is successful, return a 200 OK response
+        res.status(200).send("File uploaded successfully to s3");
+    });
 });
 
 // File download route
@@ -60,8 +74,7 @@ app.get("/files", (req, res, next) => {
             console.error(err);
             return res.status(500).send("Error listing files");
         }
-        const files = data.Contents.map((obj) => obj.Key);
-        res.status(200).json(files);
+        res.status(200).json(data.Contents);
     });
 });
 
